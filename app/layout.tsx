2f2ef0f78d0f49fb6615c9bf10af57ee4f4c99d5
Sobhane <sobhanej@gmail.com>
2025-03-11 05:17:40 +0100

    adding theme options

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,15 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { APP_DESCRIPTION, APP_NAME, APP_URL } from "@/lib/constants";
 import "./globals.css";
+import { APP_DESCRIPTION, APP_NAME, APP_URL } from "@/lib/constants";
+import { ThemeProvider } from "next-themes";
 
-const inter =  Inter({subsets: ["latin"]});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: `${APP_NAME}`,
   description: APP_DESCRIPTION,
   metadataBase: new URL("/", APP_URL),
-
 };
 
 export default function RootLayout({
@@ -18,11 +18,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${inter.className} antialiased`}
-      >
-        {children}
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className} antialiased`}>
+        <ThemeProvider
+          attribute={"class"}
+          defaultTheme="light"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,8 +1,10 @@
+"use client"
 import { ShoppingCart, UserIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { APP_NAME } from "@/lib/constants";
+import ModeToggle from "./mode-toggle";
 
 const Header = () => {
   return (
@@ -23,12 +25,13 @@ const Header = () => {
           </Link>
         </div>
       <div className="space-x-2 ">
+        <ModeToggle />
         <Button asChild variant="ghost">
           <Link href={`/cart`}>
             <ShoppingCart size={24} /> Cart
           </Link>
         </Button>
-        <Button asChild variant="ghost">
+        <Button asChild >
           <Link href={`/sign-in`}>
             <UserIcon /> Sign In
           </Link>
