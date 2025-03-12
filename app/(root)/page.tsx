42482c0765d7f2dc069be2027851db24b830e5f2
Sobhane <sobhanej@gmail.com>
2025-03-12 05:24:28 +0100

    adding product listing component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,13 @@
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
+
 
 export default async function  Home() {
-  
+  console.log(sampleData);
   return (
     <div>
-      <h1>My Store</h1>
+
+      <ProductList data={sampleData.products} title="new"/>
     </div>
   );
 }
---
+++ components/shared/product/product-list.tsx
@@ -0,0 +1,49 @@
+interface ProductList {
+  name: string;
+  slug: string;
+  category: string;
+  description: string;
+  images: string[];
+  price: number;
+  brand: string;
+  rating: number;
+  numReviews: number;
+  stock: number;
+  isFeatured: boolean;
+  banner: string;
+}
+interface ProductListProps {
+  product: [ProductList];
+}
+
+const ProductList = ({
+  data,
+  title,
+  limit,
+}: {
+  data: any;
+  title?: string;
+  limit: string;
+}) => {
+
+const limitedData = limit ? data.slice(0, limit) : data;
+
+  return (
+    <div className="my-10">
+      <h2 className="h2-bold">{title}</h2>
+      {data.length > 0 ? (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          {limitedData.map((product: any) => (
+            <div> {product.name}</div>
+          ))}
+        </div>
+      ) : (
+        <div className="text-center">
+          <p>No products found</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ProductList;
