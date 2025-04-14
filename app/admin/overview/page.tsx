0cd2c34389d862e8128ec39e98ec95812d63c3d8
Sobhane <sobhanej@gmail.com>
2025-04-14 12:26:38 +0200

    add redirection page to unauthorized

--- app/admin/overview/page.tsx
+++ app/admin/overview/page.tsx
@@ -14,21 +14,16 @@ import { BadgeDollarSign, Box, CreditCard, Users } from "lucide-react";
 import { Metadata } from "next";
 import Link from "next/link";
 import Charts from "./chart";
+import { requireAdmin } from "@/lib/auth-guard";
 
 export const metadata: Metadata = {
   title: "Admin Dashboard",
 };
 
 const AdminOverviewPage = async () => {
-  const session = await auth();
-
-  if (session?.user.role !== "admin") {
-    throw new Error("User is not authorized");
-  }
-
+  await requireAdmin()
+ 
   const summary = await getOrderSummary();
-
-  console.log(summary);
 
   return (
     <div className="space-y-2">
