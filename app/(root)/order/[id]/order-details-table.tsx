f4a39bde820e85cef6b904ff0c2c90e9fc59df68
Sobhane <sobhanej@gmail.com>
2025-04-15 20:42:37 +0200

    fix

--- app/(root)/order/[id]/order-details-table.tsx
+++ app/(root)/order/[id]/order-details-table.tsx
@@ -110,7 +110,8 @@ const OrderDetailsTable = ({
               <h2 className="text-xl pb-4">Shipping Address</h2>
               <p>{shippingAddress.fullName}</p>
               <p className="mb-2">
-                {shippingAddress.streetAddress}, {shippingAddress.city}
+                {shippingAddress.streetAddress}, {shippingAddress.city} ,{" "}
+                {shippingAddress.phoneNumber}
               </p>
               {isDelivered ? (
                 <Badge variant="secondary">
