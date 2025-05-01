55646fa9d55d96fb99d4ba42b7f501d68df9799b
Sobhane <sobhanej@gmail.com>
2025-05-01 18:38:34 +0200

    add products table to admin page

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,7 +1,7 @@
 "use server";
 import { prisma } from "@/db/prisma";
 import { prismaToJS } from "../utils";
-import { LATEST_PRODUCTS_LIMIT } from "../constants";
+import { LATEST_PRODUCTS_LIMIT, PAGE_SIZE } from "../constants";
 
 export async function getLatestProducts() {
   const data = await prisma.product.findMany({
@@ -17,4 +17,30 @@ export async function getProductBySlug(slug: string) {
   return await prisma.product.findFirst({
     where: { slug: slug },
   });
+}
+
+// Get all products
+
+export async function getAllProducts({
+  query,
+  limit = PAGE_SIZE,
+  page,
+  category,
+}: {
+  query: string;
+  limit?: number;
+  page: number;
+  category?: string;
+}) {
+  const data = await prisma.product.findMany({
+    skip: (page - 1) * limit,
+    take: limit,
+  });
+
+const dataCount = await prisma.product.count()
+
+return {
+  data,
+  totalPages: Math.ceil(dataCount)
+}
 }
